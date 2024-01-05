0800a27a0ed4a7089ea90ce2354088479541701f
carlos borrego <ingcarlosborrego@gmail.com>
2024-01-05 07:03:50 -0500

    unic commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { Formik, Form } from 'formik'
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
+import Balance from './components/Balance'
+
+const compoundInteres = (deposit, contribution, years, rate) =>{
+  let total = deposit
+  for(let i=0; i<years; i++){
+    total = (total+contribution)*(rate+1)
+  }
+  return Math.round(total)
+}
+
+const formatter = new Intl.NumberFormat('en-US',{
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+
+function App() {
+  const [balance, setBalance] = useState ('')
+  const handleSubmit =({deposit, contribution, years, rate})=> {
+    const val = compoundInteres(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format(val))
+  }
+  return (
+   <Container>
+    <Section>
+      <Formik
+        initialValues={{
+          deposit: '',
+          contribution: '',
+          years: '',
+          rate:'',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Input name='deposit' label='Depósito inicial'/>
+          <Input name='contribution' label=' Contribución anual'/>
+          <Input name='years' label='Años'/>
+          <Input name='rate' label='Interés estimado'/>
+          <Button type='submit'>Calcular</Button>
+        </Form>
+      </Formik>
+      {balance !== '' ? `Balance final: ${balance}` : null}
+      <br></br>
+      {balance !== '' ? <Balance>Balance final: {balance}</Balance> : null}
+    </Section>
+   </Container>
+  );
+}
+
+export default App;
